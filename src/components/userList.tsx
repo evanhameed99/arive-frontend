75835e16d5d1e5df26193847e9ee5cc3b1748430
Evan Hameed <63654749+evanhameed99@users.noreply.github.com>
2022-05-07 01:48:46 +0300

    style lis

--- src/components/userList.tsx
+++ src/components/userList.tsx
@@ -1,17 +1,25 @@
-import React from 'react'
-
+import classNames from 'classnames'
+import React, { useState } from 'react'
+import '../styles/main/mainTable.css'
 interface Props {
     users: any[]
 }
 
 const UserList: React.FC<Props> = ({ users }) => {
+
+    const [id, selectedId] = useState('')
+
+    const onItemClick  = (id : string) => {
+        console.log('clicked' , id)
+        selectedId(id)
+    }
     return (
         <div className='usersList'>
             {
                 users.map((user, index) => {
                     return (
-                        <ul className='usersUl' key={user._id}>
-                            <li>{user.name}</li>
+                        <ul className='usersUl'  >
+                            <li className={classNames('' , { 'liClicked' : id == user._id})} onClick={()=> onItemClick(user._id)} key={user._id} >{user.name}</li>
                         </ul>
                     )
                 })
