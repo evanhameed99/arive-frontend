0fc0557c8b99937df0264bc99b7cee489d3c28bc
Evan Hameed <63654749+evanhameed99@users.noreply.github.com>
2022-05-06 19:40:15 +0300

    set color pallete

---
+++ src/components/main.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../styles/main/mainTable.css'
+const Main = () => {
+  return (
+    <div className='mainTable'>
+            hello
+    </div>
+  )
+}
+
+export default Main
