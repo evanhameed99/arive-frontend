0980a4ca85ad6ead7f45f9d591909850acfac7ae
Evan Hameed <63654749+evanhameed99@users.noreply.github.com>
2022-05-07 19:23:08 +0300

    add type to Header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import '../styles/main/mainTable.css'
-const Header = () => {
+const Header: React.FC = () => {
   return (
     <div className='header'>
-        <h1>User Hobbies</h1>
+      <h1>User Hobbies</h1>
     </div>
   )
 }
