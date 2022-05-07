f4d19b5f361adc3716d0925fabc8e82a8c9b2948
Evan Hameed <63654749+evanhameed99@users.noreply.github.com>
2022-05-08 01:26:19 +0300

    install test packages

--- src/__tests__/header.test.tsx
+++ src/__tests__/header.test.tsx
@@ -6,7 +6,6 @@ import Header from '../components/header';
 it('Should render a header!' , ()=>{
     const component = shallow(<Header />);
     // eslint-disable-next-line testing-library/no-debugging-utils
-    console.log(component.debug());
-    const wrapper = component.find('h1');
+    const wrapper = component.find('[data-test="header"]');
     expect(wrapper.length).toBe(1);
 })
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import '../styles/main/mainTable.css'
 const Header: React.FC = () => {
   return (
-    <div className='header'>
+    <div className='header' data-test='header'>
       <h1>User Hobbies</h1>
     </div>
   )
